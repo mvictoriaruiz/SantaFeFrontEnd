2a6d3fa06dd0e620e20071b980208521ca48d1b1
Victoria Ruiz <115724215+mvictoriaruiz@users.noreply.github.com>
2022-11-09 15:05:51 -0300

    Add files via upload
    Práctico 9

--- js/javascript.js
+++ js/javascript.js
@@ -1,11 +1,12 @@
 
 function cambiarfondo1() {
+  document.body.style.backgroundImage = "none";
   document.body.style.backgroundColor = "black" ;
   document.body.style.color= "white";
   document.getElementById('aside').style.backgroundColor = "black";
-  document.getElementById('main').style.backgroundColor = "black";
+  document.getElementById('objeto2').style.backgroundColor = "black";
   document.getElementById('footer').style.backgroundColor = "white";
-  document.getElementById('section').style.color= "white";
+  document.getElementById('bar').style.color= "white";
 }
 
 function nombre() {
