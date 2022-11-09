a8e95135cec7a2459f46f4042bb38bdd8a60bee8
Victoria Ruiz <115724215+mvictoriaruiz@users.noreply.github.com>
2022-11-09 13:51:33 -0300

    Add files via upload

---
+++ js/javascript.js
@@ -0,0 +1,13 @@
+
+function cambiarfondo1() {
+  document.body.style.backgroundColor = "black" ;
+  document.body.style.color= "white";
+  document.getElementById('aside').style.backgroundColor = "black";
+  document.getElementById('main').style.backgroundColor = "black";
+  document.getElementById('footer').style.backgroundColor = "white";
+  document.getElementById('section').style.color= "white";
+}
+
+function nombre() {
+  alert("María Victoria Ruiz Gómez. Email: mvictoria.ruizgomez@gmail.com" )
+  }
